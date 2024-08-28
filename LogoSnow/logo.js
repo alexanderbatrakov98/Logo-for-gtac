db3b0e027509ab6b69505da1414dc0a94b6cf388
Alexander Batrakov <163431404+alexanderbatrakov98@users.noreply.github.com>
2024-08-28 21:13:31 +0300

    Update logo.js

--- LogoSnow/logo.js
+++ LogoSnow/logo.js
@@ -1,1 +1,27 @@
+"use strict";
 
+let logoFont = null;
+
+bindEventHandler("OnResourceReady", thisResource, function(event, resource) {
+    let fontStream = openFile("DSSnowfall.ttf"); 
+    if (fontStream != null) {
+        logoFont = lucasFont.createFont(fontStream, 40.0); 
+        fontStream.close();
+    }
+    forceSnowing(true);
+});
+
+bindEventHandler("OnResourceStop", thisResource, (event,resource) => {
+	forceSnowing(false);
+});
+
+addEventHandler("OnDrawnHUD", function(event) {
+    if (logoFont != null) {
+        let logoText = "Connected freeroam";
+        
+        let hudX = 1300; 
+        let hudY = 10;  
+
+        logoFont.render(logoText, [hudX, hudY], 0, 0.5, 0.0, logoFont.size, COLOUR_WHITE, false, false, false, true);
+    }
+});
